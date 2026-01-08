1c1a0303de5195b69f29bffb2add20dd1b4adf59
fanboynet <48902228+fanboynet@users.noreply.github.com>
2026-01-08 09:05:34 +0800

    Add MBC1 cartridge support

---
+++ src/Emulator.pas
@@ -0,0 +1,90 @@
+unit Emulator;
+
+interface
+
+uses
+  System.SysUtils,
+  Common,
+  CPU,
+  Memory,
+  PPU,
+  APU,
+  Cartridge;
+
+type
+  TGBEmulator = class
+  private
+    FCPU: TCPU;
+    FMemory: TMemory;
+    FPPU: TPPU;
+    FAPU: TAPU;
+    FCartridge: TCartridge;
+  public
+    constructor Create;
+    destructor Destroy; override;
+    procedure Reset;
+    function LoadCartridge(const FileName: string): Boolean;
+    procedure Step;
+    procedure RunFrame;
+  end;
+
+implementation
+
+constructor TGBEmulator.Create;
+begin
+  inherited Create;
+  FMemory := TMemory.Create;
+  FPPU := TPPU.Create;
+  FCartridge := TCartridge.Create;
+  FMemory.AttachPPU(FPPU);
+  FMemory.AttachCartridge(FCartridge);
+  FCPU := TCPU.Create(FMemory);
+  FAPU := TAPU.Create;
+  Reset;
+end;
+
+destructor TGBEmulator.Destroy;
+begin
+  FCartridge.Free;
+  FAPU.Free;
+  FCPU.Free;
+  FPPU.Free;
+  FMemory.Free;
+  inherited Destroy;
+end;
+
+procedure TGBEmulator.Reset;
+begin
+  FMemory.Reset;
+  FCPU.Reset;
+  FPPU.Reset;
+  FAPU.Reset;
+end;
+
+function TGBEmulator.LoadCartridge(const FileName: string): Boolean;
+begin
+  Result := FCartridge.LoadFromFile(FileName);
+end;
+
+procedure TGBEmulator.Step;
+var
+  Cycles: Integer;
+begin
+  Cycles := FCPU.Step;
+  FPPU.Step(Cycles);
+  FAPU.Step(Cycles);
+end;
+
+procedure TGBEmulator.RunFrame;
+var
+  CycleCount: Integer;
+begin
+  CycleCount := 0;
+  while CycleCount < FrameCycles do
+  begin
+    Step;
+    Inc(CycleCount, 4);
+  end;
+end;
+
+end.
---
+++ src/Memory.pas
@@ -0,0 +1,129 @@
+unit Memory;
+
+interface
+
+uses
+  Common, PPU, Cartridge;
+
+type
+  TMemory = class
+  private
+    FData: array[0..MemorySize - 1] of TByte;
+    FPPU: TPPU;
+    FCartridge: TCartridge;
+  public
+    procedure AttachPPU(APPU: TPPU);
+    procedure AttachCartridge(ACartridge: TCartridge);
+    procedure Reset;
+    function ReadByte(Address: TWord): TByte;
+    procedure WriteByte(Address: TWord; Value: TByte);
+    procedure LoadBootRom(const Data: TBytes);
+    procedure LoadCartridge(const Data: TBytes);
+  end;
+
+implementation
+
+procedure TMemory.AttachPPU(APPU: TPPU);
+begin
+  FPPU := APPU;
+end;
+
+procedure TMemory.AttachCartridge(ACartridge: TCartridge);
+begin
+  FCartridge := ACartridge;
+end;
+
+procedure TMemory.Reset;
+begin
+  FillChar(FData, SizeOf(FData), 0);
+end;
+
+function TMemory.ReadByte(Address: TWord): TByte;
+begin
+  if Assigned(FCartridge) then
+  begin
+    if (Address <= $7FFF) or ((Address >= $A000) and (Address <= $BFFF)) then
+      Exit(FCartridge.ReadByte(Address));
+  end;
+  if Assigned(FPPU) then
+  begin
+    if (Address >= $8000) and (Address <= $9FFF) then
+      Exit(FPPU.ReadVRAM(Address));
+    if (Address >= $FE00) and (Address <= $FE9F) then
+      Exit(FPPU.ReadOAM(Address));
+    if (Address >= $FF40) and (Address <= $FF4B) then
+      Exit(FPPU.ReadRegister(Address));
+    if (Address >= $FF68) and (Address <= $FF6B) then
+      Exit(FPPU.ReadRegister(Address));
+    if Address = $FF4F then
+      Exit(FPPU.ReadRegister(Address));
+  end;
+  Result := FData[Address];
+end;
+
+procedure TMemory.WriteByte(Address: TWord; Value: TByte);
+begin
+  if Assigned(FCartridge) then
+  begin
+    if (Address <= $7FFF) or ((Address >= $A000) and (Address <= $BFFF)) then
+    begin
+      FCartridge.WriteByte(Address, Value);
+      Exit;
+    end;
+  end;
+  if Assigned(FPPU) then
+  begin
+    if (Address >= $8000) and (Address <= $9FFF) then
+    begin
+      FPPU.WriteVRAM(Address, Value);
+      Exit;
+    end;
+    if (Address >= $FE00) and (Address <= $FE9F) then
+    begin
+      FPPU.WriteOAM(Address, Value);
+      Exit;
+    end;
+    if (Address >= $FF40) and (Address <= $FF4B) then
+    begin
+      FPPU.WriteRegister(Address, Value);
+      Exit;
+    end;
+    if (Address >= $FF68) and (Address <= $FF6B) then
+    begin
+      FPPU.WriteRegister(Address, Value);
+      Exit;
+    end;
+    if Address = $FF4F then
+    begin
+      FPPU.WriteRegister(Address, Value);
+      Exit;
+    end;
+  end;
+  FData[Address] := Value;
+end;
+
+procedure TMemory.LoadBootRom(const Data: TBytes);
+var
+  Index: Integer;
+begin
+  for Index := 0 to High(Data) do
+  begin
+    if Index >= MemorySize then
+      Break;
+    FData[Index] := Data[Index];
+  end;
+end;
+
+procedure TMemory.LoadCartridge(const Data: TBytes);
+var
+  Index: Integer;
+begin
+  for Index := 0 to High(Data) do
+  begin
+    if Index >= MemorySize then
+      Break;
+    FData[Index] := Data[Index];
+  end;
+end;
+
+end.
